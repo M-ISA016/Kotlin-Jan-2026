5f2c75cbc94d18ab38a1b257704b42cadbbbd50c
Maria Isabella <isabellaac837@gmail.com>
2026-01-04 23:36:02 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,26 @@
+// revision (03.01.2026)
+fun main() { // Idea: kotlin course
+    val name = "Maria"
+    val age = 17
+    val firstNameLetter = 'M'
+    val weight = 46.0
+    val isAProgrammer = true
+
+    println("Name: $name;")
+    println("Age: $age;")
+    println("first name letter: $firstNameLetter;")
+    println("Weight: $weight;")
+    println("Is a programmer? $isAProgrammer.")
+
+    dog()
+}
+
+fun dog() { // Idea: Gemini
+    val name = "Bill"
+    val age = 3
+    val humanAge = (age * 7)
+    
+    println("$name is $age years old, which is equivalent to $humanAge in human age.")
+}
+
+
