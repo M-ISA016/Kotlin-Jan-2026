b520df15a0127367ae340a4b2435925eee7e9d92
Maria Isabella <isabellaac837@gmail.com>
2026-01-20 17:15:31 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val name = "Maria"
+    val ageIn2025 = 17
+    val ageIn2050 = ageIn2025+25
+    println("Hello, $name! In 2050, you will be $ageIn2050. Your name has ${name.length} characters.")
+}
