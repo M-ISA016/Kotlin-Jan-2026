9e46a5a15f4ea76f16440b8a65a770800849da08
Maria Isabella <isabellaac837@gmail.com>
2026-01-22 16:29:39 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,11 @@
+fun main() { // Remember to use "in"
+    println("Insert the current temperature:")
+    val temperature = readln().toInt()
+
+    when(temperature) {
+      in -100..14 -> println("Cold")
+        in 15..25 -> println("Pleasant")
+        in 25..100 -> println("Hot")
+    }
+}
+
