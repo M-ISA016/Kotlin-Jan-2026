8ff85ef7d15244f351bcb70150d4ce9f23464e32
Maria Isabella <isabellaac837@gmail.com>
2026-01-23 00:28:21 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Salary Bonus Calculator. Put your number of years working in the enterprise.")
+    val enterpriseYears = readln().toInt()
+
+    when (enterpriseYears) {
+        in 0..1 -> println("5% Bonus!")
+        in 2..5 -> println("10% Bonus!")
+        in 6..10 -> println("15% Bonus!")
+        else -> println("20% Bonus!")
+    }
+}
